3b30d91bf5cde67647938aada02dbc72a5b1d2f1
Lenayah <lfanesie@gmail.com>
2023-09-28 13:48:44 -0400

    update

--- Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,34 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
+func introduceMyself(value: String){
+   print(value)
+}
+
+introduceMyself(value: "Lenayah")
+
 
 
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
-
-
+func magicEightBall(){
+    let randomNum = Int.random(in: 0...4)
+    switch randomNum{
+    case 0:
+        print("Hi")
+    case 1:
+        print("Bye")
+    case 2:
+        print("Hello")
+    case 3:
+        print("Goodbye")
+    default:
+        print("Nothing")
+    }
+    
+    
+}
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,17 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
-
+func greeting(name: String){
+    print("Hi, \(name)! How are you?")
+}
+greeting(name: "Dan")
 
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
-
+func f(value1: Int, value2: Int) -> Int {
+    let sum = value1 * value2 + 2
+    return sum
+}
+print(f(value1: 10, value2: 3))
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
  */
